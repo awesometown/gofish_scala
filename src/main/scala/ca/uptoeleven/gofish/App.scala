ba0784b44a761f957a77238e8517c7faf9e75c3d
John Price <joprice@ea.com>
2013-11-22 15:55:48 -0500

    Playing with gamelogic FSM

--- src/main/scala/ca/uptoeleven/gofish/App.scala
+++ src/main/scala/ca/uptoeleven/gofish/App.scala
@@ -1,8 +1,31 @@
 package ca.uptoeleven.gofish
 
+import akka.actor.{ ActorRef, ActorSystem, Props, Actor, Inbox }
+
 object App {
   
   def main(args : Array[String]) {
     var (card, deck) = (new Deck).shuffle.dealCard
+    println(card)
+    
+    val system = ActorSystem("mySystem")
+    val gameLogic = system.actorOf(Props[GameLogic], "gameLogic")
+    val dummy1 = system.actorOf(Props[Dummy], "dummy1")
+    val dummy2 = system.actorOf(Props[Dummy], "dummy2")
+    
+    val inbox = Inbox.create(system)
+    gameLogic.tell(Join, dummy1)
+    gameLogic.tell(Join, dummy2)
+    
+    gameLogic.tell(Play(new Card(Clubs, 1)), dummy1)
+    gameLogic.tell(Play(new Card(Clubs, 2)), dummy2)
+    
+    system.shutdown
+  }
+  
+  class Dummy extends Actor {
+    def receive = {
+      case whatever => println(whatever)
+    }
   }
 }
